9f22ba36a36d00069e984182a780d44943b6fe4b
Vitor <vitormarinheiro9@gmail.com>
2025-02-01 14:49:28 -0300

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <h1>Login</h1>
+
+    <form action="/login" method="post">
+        @csrf
+
+        <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}" />
+        <input type="password" name="password" placeholder="Senha" />
+
+        <button>Logar</button>
+    </form>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::post('/login', [LoginController::class, 'login']);
+
+
+Route::get('/dashboard', fn () => 'dashboard :: '.auth()->id())
+->middleware('auth')->name('dashboard');
