a1a25804e7776207d168cdb55ba013cf2cdac0e0
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2025-02-03 07:45:26 -0300

    adicionando logout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,12 +9,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
 
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+});
 
+Route::get('/logout', LogoutController::class)->middleware('auth')->name('logout');
 
-Route::get('/dashboard', fn () => 'dashboard :: '.auth()->id())
-->middleware('auth')->name('dashboard');
+Route::get('/dashboard', fn() => 'dashboard :: ' . auth()->id())
+    ->middleware('auth')->name('dashboard');
