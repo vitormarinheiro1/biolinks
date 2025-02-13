bb84addb2f9163db39bc75d72887804a72fd8fbe
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2025-02-13 15:43:15 -0300

    feat: add instalação do tailwindcss e daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,16 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+        extend: {},
     },
-    plugins: [],
-};
+    plugins: [
+        daisyui
+    ],
+}
